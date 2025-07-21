24771ae7bd02081201671d96ca75528194ffe05f
Your Name <your.email@example.com>
2025-07-21 17:35:57 +0530

    update,delete job updated

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -14,7 +14,7 @@ const router = express.Router();
 router.post("/", protect, authorize("jobprovider"), createJob);
 router.get("/", protect, authorize("jobprovider"), getJobs);
 router.get("/:id", protect, authorize("jobprovider"), getJob);
-router.put("/:id", protect, authorize("jobprovider"), updateJob);
+router.put("/:id", protect, authorize("jobprovider"), updateJob)
 router.delete("/:id", protect, authorize("jobprovider"), deleteJob);
 
 module.exports = router;
